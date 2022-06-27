eaee6ec14df801553aecd5c12c45f106c80e0ed2
adityanaidu16 <adityanaidu16344@gmail.com>
2022-06-27 18:28:33 -0500

    nationals

--- app.py
+++ app.py
@@ -1,64 +1,144 @@
 from flask import Flask, render_template, request
-import csv
-from datetime import datetime
+import requests
+from geopy.geocoders import Nominatim
+import time
+import os
 
 # import python functions from helpers.py
-from helpers import apology, time_in_range
+from helpers import apology, convertType, formatAttractionsList, formatImage
 
 # Configure flask application
 app = Flask(__name__)
 
-# render index.html by default
+# import Google Maps API Key (stored as hidden environment variable)
+from dotenv import load_dotenv
+load_dotenv()
+API_KEY = os.environ.get('API_KEY')
+
+# render index.html by default 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-# function for converting 24 hour time (from spreadsheet) to 12 hour time (to display)
-@app.template_filter()
-def convert_time(time):
-    new_time = datetime.strptime(time, "%H:%M")
-    new_time = new_time.strftime("%I:%M %p")
-
-    # remove redundant zero if possible
-    if (new_time[0] == '0'):
-        new_time = new_time[1:]
-
-    # return 12-hour-formatted time
-    return new_time
+    return render_template('index.html', API_KEY=API_KEY)
 
 # function for filtering attractions
 @app.route("/", methods=["GET", "POST"])
 def attractions():
-    # User reach route via POST (as by submitting a form via POST)
-    if request.method == "POST":
+    # User reach route via filter section POST (as by submitting a form via POST)
+    if request.method == "POST" and 'category_response' in request.form:
+        
+        # toggle "show more" button visibility
+        show_more = True
+
+        # initialize global variables
+        global category, cost, location, rating, open, url, attractions_list, request_time
 
         # request user input from HTML form
         category = request.form.get("category_response")
         cost = request.form.get("cost_response")
         location = request.form.get("location_response")
         rating = request.form.get("rating_response")
-        time = request.form.get("time_response")
+        open = request.form.get("open_response")
+        if open == 'on':
+            open = "&opennow=true"
+        else:
+            open = ""
 
-        # open csv file containing attractions' information
-        with open('attractions.csv') as csvfile:
+        try:
+            # create coordinates based on inputted location
+            geolocator = Nominatim(user_agent="TravelMonkey")
+            locator = geolocator.geocode(location)
 
-            # read csv file
-            attractions_csv = csv.reader(csvfile)
-            header = next(attractions_csv)
+            lat = str(locator.latitude)
+            long = str(locator.longitude)
+        except:
+            # return error if location does not exist (exception occurs)
+            return apology("Invalid Location", 400)
 
-            # filter row-by-row for matching parameters
-            results = [row for row in attractions_csv if (category == row[2] or category == "Any") and (cost == row[3] or cost == "Any") and (location.lower() == row[4].lower() or location == "") and (row[5] >= rating or rating == "Any")and (time == "" or time_in_range(row[6], row[7], time) == True)]
+        # set API endpoint url
+        url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
+
+        # first request
+        r = requests.get(url + 'location=' + lat + "," + long + "&type=" + category + open + "&key=" + API_KEY)
+        attractions_list = r.json()
+
+        # print(url + 'location=' + str(locator.latitude) + "," + str(locator.longitude) + "&type=" + category + open + "&key=" + API_KEY)
+
+        # format list of attractions
+        attractions_list = formatAttractionsList(attractions_list, API_KEY)
+
+        # filter results
+        results = [row for row in attractions_list['results'] if (str(row['rating']) >= rating or rating == "Any") and (int(cost) >= row['price_level']) and (row['user_ratings_total'] > 600)]
+        results = sorted(results, key=lambda d: d['user_ratings_total'], reverse=True)
+
+        # return error message if no results are found
+        if not results:
+            return apology("No results found", 400)
+
+        # start timer to prepare for second request
+        request_time = time.time()
+
+        # render displayattractions.html and pass results
+        return render_template('displayattractions.html', results=results, convertType=convertType, formatImage=formatImage, show_more=show_more, API_KEY=API_KEY)
+
+    # User reach route via show more button POST (as by submitting request via POST)
+    elif request.method == "POST" and 'ShowMoreButton'in request.form:
+
+        # toggle "show more" button visibility
+        show_more = False
+
+        # if more results are available
+        if('next_page_token' in attractions_list):
+
+            # wait for cooldown period to end
+            if (time.time() - request_time < 2):
+                time.sleep(2 - (time.time() - request_time))
+            else:
+                pass
+
+            # second request
+            r2 = requests.get(url + 'pagetoken=' + attractions_list['next_page_token'] + "&key=" + API_KEY)
+            request2 = r2.json()
+
+            # format second list of attractions
+            request2 = formatAttractionsList(request2, API_KEY)
+            
+            # append to existing list
+            attractions_list['results'] = attractions_list['results'] + request2['results']
+
+            # if even more results are available
+            if('next_page_token' in request2):
+                
+                # wait for cooldown period to end
+                time.sleep(2)
+
+                # third request
+                r3 = requests.get(url + 'pagetoken=' + request2['next_page_token'] + "&key=" + API_KEY)
+                request3 = r3.json()
+
+                # format third list of attractions
+                request3 = formatAttractionsList(request3, API_KEY)
+                
+                # append to existing list
+                attractions_list['results'] = attractions_list['results'] + request3['results']
+
+        # format list of attractions
+        attractions_list = formatAttractionsList(attractions_list, API_KEY)
         
+        # filter results
+        results = [row for row in attractions_list['results'] if (str(row['rating']) >= rating or rating == "Any") and (int(cost) >= row['price_level']) and (row['user_ratings_total'] > 600)]
+        
+        # sort results from most user reviews (most visited attractions)
+        results = sorted(results, key=lambda d: d['user_ratings_total'], reverse=True)
+
         # return error message if no results are found
         if not results:
             return apology("No results found", 400)
 
         # render displayattractions.html and pass results
-        return render_template('displayattractions.html', results=results, convert_time=convert_time)
-
+        return render_template('displayattractions.html', results=results, convertType=convertType, formatImage=formatImage, show_more=show_more, API_KEY=API_KEY)
     else:
-        # render index.html until HTML form is submitted
-        return render_template('index.html')
+        # render index.html until HTML filter form is submitted
+        return render_template('index.html', API_KEY=API_KEY)
 
 # render help.html when Help is clicked on nav-bar
 @app.route("/help")
--- helpers.py
+++ helpers.py
@@ -1,24 +1,85 @@
 from flask import render_template
-import datetime
 
+# error message
 def apology(message, code):
 
     # Render message as an apology to user, pass error code and message
     return render_template("apology.html", code=code, message=message)
     
+# format attraction types
+def convertType(type):
+    if type == "amusement_park":
+        return "Amusement Park"
+    elif type =="aquarium":
+        return "Aquarium"
+    elif type == "campground":
+        return "Campground"
+    elif type == "museum":
+        return "Museum"
+    elif type == "park":
+        return "Park"
+    elif type == "restaurant":
+        return "Restaurant"
+    elif type == "shopping_mall":
+        return "Shopping"
+    elif type =="stadium":
+        return "Stadium"
+    elif type =="tourist_attraction":
+        return "Tourist Attraction"
+    elif type =="zoo":
+        return "Zoo"
+    else:
+        return None
 
-# function for determining if a given time is within a given range
-def time_in_range(start, end, current):
+# format images for high resolution/quality
+def formatImage(img):
+    return img.replace('maxwidth=400', 'maxwidth=1280')
 
-    # split times into hours and minutes
-    start = start.split(":")
-    end = end.split(":")
-    current = current.split(":")
+# format list of attractions/results
+def formatAttractionsList(list, API_KEY):
+    for item in list['results']:
+        # if price level exists, don't change
+        try:
+            item['price_level'] = item['price_level']
 
-    # input hours and minutes into datetime format
-    start = datetime.time(int(start[0]), int(start[1]))
-    end = datetime.time(int(end[0]), int(end[1]))
-    current = datetime.time(int(current[0]), int(current[1]))
+        # if price level doesn't exist, change it to the following:
+        except:
+            # set price level of parks to free
+            if 'park' in item['types'] or 'campground' in item['types']:
+                item['price_level'] = 0
+            
+            # set price level of museums, aquariums, and amusement parks to 3
+            elif 'museum' in item['types'] or 'aquarium' in item['types'] or 'amusement_park' in item['types']:
+                item['price_level'] = 3
+            
+            # set everything else to 2
+            else:
+                item['price_level'] = 2
+        
+        # format image source url
+        try:
+            item['image_source_primary'] = str("https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=" + item['photos'][0]['photo_reference'] + "&key=" + API_KEY)
+        except:
+            item['image_source_primary'] = "..."
 
-    # determine whether given time is within datetime-formatted range
-    return start <= current <= end
+        # ensure rating exists (to avoid further errors when filtering)
+        try:
+            item['rating'] = item['rating']
+        except:
+            item['rating'] = 0
+        
+        # ensure # of ratings exists
+        try:
+            item['user_ratings_total'] = item['user_ratings_total']
+        except:
+            item['user_ratings_total'] = 0
+
+        # format image attribution
+        try:
+            item['image_attribution'] = str(item['photos'][0]['html_attributions']).replace("<a", "<a class='mb-0 text-gray-50 fs-7' target='_blank'")
+            item['image_attribution'] = item['image_attribution'][2:-2]
+        except:
+            item['image_attribution'] = "<p class='mb-0 mt-0 text-gray-50 fs-7'>Unavailable</p>"
+
+    # return formatted list of attractions
+    return list
